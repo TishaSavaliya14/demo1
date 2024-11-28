0925041cbd55ce23ba56d7047b55a03f94d91a59
TishaSavaliya14 <tisha.micra@gmail.com>
2024-11-28 09:44:28 +0530

    first commmit

--- test/src/App.js
+++ test/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn reactjs
+          Learn Reactjs
         </a>
       </header>
     </div>
