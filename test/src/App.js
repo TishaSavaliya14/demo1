3f71c94065a0ce779cc58fe8d8e3b6176a2699ba
TishaSavaliya14 <tisha.micra@gmail.com>
2024-12-02 17:35:39 +0530

    first commit

--- test/src/App.js
+++ test/src/App.js
@@ -1,23 +1,51 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
+  const [data, setdata] = useState([]);
+  const [inputdata, setinputdata] = useState({
+    title: "",
+  });
+
+  // fetch("https://jsonplaceholder.typicode.com/todos")
+  //   .then((res) => res.json())
+  //   .then((d) => setdata(d));
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((d) => setdata(d.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  function handlesubmit() {
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", {
+        title: inputdata.title,
+      })
+      .then((d) => console.log(d.data))
+      .catch((err) => console.log(err));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React23456
-        </a>
-      </header>
+      <input
+        type="text"
+        name="title"
+        value={inputdata.title}
+        onChange={(e) => setinputdata({ ...inputdata, title: e.target.value })}
+      />
+      <button type="button" onClick={handlesubmit}>
+        submit
+      </button>
+      <h1>data get</h1>
+      {data.map((i) => {
+        return (
+          <>
+            <p>{i.title}</p>
+          </>
+        );
+      })}
     </div>
   );
 }
